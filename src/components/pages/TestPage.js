0a348d29379eb514804178a25db43ac7932addd0
oHoangVietDuc <hoang.viet.duc@framgia.com>
2018-04-17 15:29:56 +0700

    Updates

--- src/components/pages/TestPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const TestPage = () => (
-  <h1>Test page</h1>
-);
-
-export default TestPage;
--- src/utils/SetAuthorizationHeader.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default (key = null) => {
-  if (key) {
-    axios.defaults.headers.common.authorization = `Bearer ${key}`;
-  } else {
-    delete axios.defaults.headers.common.authorization;
-  }
-};

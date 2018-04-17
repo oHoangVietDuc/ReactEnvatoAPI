fc26dc2ec2d511ab57d87175a22d914199ee94b5
oHoangVietDuc <hoang.viet.duc@framgia.com>
2018-04-17 09:45:56 +0700

    Updates

--- src/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import user from './reducers/user';
-import statement from './reducers/statement';
-
-export default combineReducers({
-  user,
-  statement
-});
